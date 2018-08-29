5ad0491c2393c48b7ad33ea72ae8e27c467cc184
HanyongLee <suneye15@naver.com>
2018-08-29 08:34:39 +0000

    second commit

--- app.py
+++ app.py
@@ -3,10 +3,10 @@
 from flask import Flask, render_template, request
 import random
 import csv
-
-
+from faker import Faker
 
 app = Flask(__name__)
+fake = Faker('ko_KR') # 한글로
 
 @app.route("/")
 def index():
@@ -21,7 +21,7 @@ def result():
     match = random.randrange(50, 101)
     #names라는 배열에 입력된 두 이름을 넣는다.
     # 'names.csv' 파일을 만들어서 저장한다.
-    f = open('names.csv', 'a', encoding="utf-8" )
+    f = open('names.csv', 'a', encoding="utf-8")
     a = csv.writer(f)
     a.writerow([name1, name2]) #csv는 2차원 배열(리스트)
     f.close()
@@ -35,5 +35,14 @@ def admin():
     names = rr
     return render_template('admin.html', names = names)
     
+@app.route("/ffaker")
+def ffaker():
+    name = fake.name()
+    job = fake.job()
+    address = fake.address()
+    text = fake.text()
     
+    return render_template('ffaker.html', name = name, address = address, job=job, text=text)
+
+
 #app.run(host='0.0.0.0', port='8080', debug=True) #위 run 누르면 됨
